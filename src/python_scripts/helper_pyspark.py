4359c11c1ad4565d539a46687741a7fc5e300729
riddhi_gohil <gohilriddhi21@gmail.com>
2025-04-27 18:32:04 -0700

    optmised helpers

--- src/python_scripts/helper_pyspark.py
+++ src/python_scripts/helper_pyspark.py
@@ -1,6 +1,6 @@
 from pyspark.sql.functions import (
     col, lit, expr, month, year, trunc, date_format, size, sequence,
-    explode, when, dayofweek
+    explode, when, dayofweek, broadcast
 )
 from pyspark.sql import functions as F
 from pyspark.sql import Window
@@ -16,22 +16,23 @@ def fun_fill_day_details(df_sales_monthly):
     df_with_latest = df.withColumn("year_ref", F.max("month").over(window_spec_latest))
 
     df_main = df_with_latest.groupBy("jan1_day", "month_num", "leap_year", "year_ref") \
-        .agg(F.expr("percentile_approx(days, 0.5)").cast("int").alias("days_max"))
+        .agg(F.expr("percentile_approx(days, 0.5, 10000)").cast("int").alias("days_max"))
 
-    df_manual_add_1 = df_main.filter(
-        (col("month_num") <= 2) & (~col("leap_year")) & (col("jan1_day") == "Wednesday")
-    ).withColumn("leap_year", lit(True)) \
-     .withColumn("days_max", col("days_max") + when(col("month_num") == 2, 1).otherwise(0))
-
-    df_manual_add_2 = df_main.filter(
-        (col("month_num") > 2) & (~col("leap_year")) & (col("jan1_day") == "Thursday")
-    ).withColumn("leap_year", lit(True)) \
-     .withColumn("jan1_day", lit("Wednesday"))
-
-    df_manual_combined = df_manual_add_1.union(df_manual_add_2)
-
-    df_manual_final = df_manual_combined.join(df_main, ["jan1_day", "month_num", "leap_year"], "anti")
-
+    df_manual_add = df_main.filter(
+        ((col("month_num") <= 2) & (~col("leap_year")) & (col("jan1_day") == "Wednesday")) |
+        ((col("month_num") > 2) & (~col("leap_year")) & (col("jan1_day") == "Thursday"))
+    ).withColumn(
+        "leap_year", lit(True)
+    ).withColumn(
+        "jan1_day", when((col("month_num") > 2) & (col("jan1_day") == "Thursday"), lit("Wednesday")).otherwise(col("jan1_day"))
+    ).withColumn(
+        "days_max", col("days_max") + when((col("month_num") == 2), 1).otherwise(0)
+    )
+    
+    # can change later depedinding on the size of the df_main. 
+    # should or should not broadcast the df_main
+    df_manual_final = df_manual_add.join(broadcast(df_main), ["jan1_day", "month_num", "leap_year"], "anti")
+    
     final_df = df_main.unionByName(df_manual_final)
 
     print("\n\nfun_fill_day_details:")
@@ -50,8 +51,11 @@ def fun_reinvestment_days_lost(df_reinvestment, df_sales_monthly):
         .withColumn("jan1_day", date_format(trunc("month", "year"), "EEEE")) \
         .withColumn("month_num", month("month")) \
         .withColumn("leap_year", ((year("month") % 4 == 0) & ((year("month") % 100 != 0) | (year("month") % 400 == 0)))) \
-        .join(df_day_details, on=["jan1_day", "month_num", "leap_year"], how="inner")
-
+        # .join(df_day_details, on=["jan1_day", "month_num", "leap_year"], how="inner") \
+        # .drop("month_idx", "month_between")
+    
+    df = df.join(broadcast(df_day_details), on=["jan1_day", "month_num", "leap_year"], how="inner")
+        
     df = df.withColumn(
         "days_lost",
         when(
@@ -89,13 +93,10 @@ def fun_annualized_inflation(live_forecast=True): return 1.01 if live_forecast e
 def fun_long_run_inflation(): return 1.025
 
 def fun_sales_days_between(start_col, end_col):
-    return size(expr(f"filter(sequence({start_col}, {end_col}, interval 1 day), x -> date_format(x, 'EEEE') != 'Sunday')"))
-
-def fun_sales_days_between(start_col, end_col):
     return size(
         F.filter(
             sequence(start_col, end_col),
-            lambda x: dayofweek(x) != 1  # Sunday = 1
+            lambda x: dayofweek(x) != 1  
         )
     )
 
